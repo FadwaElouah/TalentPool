b4c149620a93c37c951d1e778f1d2de8754f8415
FadwaElouah <fadwaelouah@gmail.com>
2025-03-25 12:35:53 +0000

    add folder interface

--- talent/database/migrations/2025_03_25_113628_create_users_table.php
+++ talent/database/migrations/2025_03_25_113628_create_users_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_roles', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('name');
-            $table->string('color')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('users');
     }
 };
