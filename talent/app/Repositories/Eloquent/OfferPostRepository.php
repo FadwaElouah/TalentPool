f96a9992f4b906444c4ede994cbf5f93f84d353f
FadwaElouah <fadwaelouah@gmail.com>
2025-03-26 14:02:21 +0000

    add file offer post

--- talent/app/Repositories/Eloquent/OfferPostRepository.php
+++ talent/app/Repositories/Eloquent/OfferPostRepository.php
@@ -5,11 +5,43 @@ namespace App\Repositories\Eloquent;
 use App\Models\OfferPost;
 use App\Repositories\Interfaces\OfferPostRepositoryInterface;
 
-class OfferPostRepository extends BaseRepository implements OfferPostRepositoryInterface
+class OfferPostRepository implements OfferPostRepositoryInterface
 {
+    protected $model;
+
     public function __construct(OfferPost $model)
     {
-        parent::__construct($model);
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $record = $this->find($id);
+        if ($record) {
+            $record->update($data);
+            return $record;
+        }
+        return false;
+    }
+
+    public function delete($id)
+    {
+        return $this->model->destroy($id);
     }
 
     public function getActiveOffers()
